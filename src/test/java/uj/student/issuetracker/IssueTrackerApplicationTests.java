4759ccdf91ec23ea079f87883c121aed1b732f9d
krzysztof Wydrzynski <krzysztof.wydrzynski99@gmail.com>
2023-04-01 13:58:01 +0200

    First commit

---
+++ src/test/java/uj/student/issuetracker/IssueTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package uj.student.issuetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
