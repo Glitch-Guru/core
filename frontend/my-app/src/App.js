a80c5ed0ac6636fb9d9cbca11540cd3ccea8ab86
unknown <angell.siwek36@gmail.com>
2023-05-20 09:26:02 +0200

    Adding creating ticket component

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Registration from './Registration';
 import Logging from "./Logging";
+import CreateTicket from "./CreateTicket";
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
                 <Routes>
                     <Route path="/register" element={<Registration />} />
                     <Route path="/login" element={<Logging />} />
+                    <Route path="/createticket" element={<CreateTicket/>} />
                     {/* Define more routes for other components/pages */}
                 </Routes>
             </div>
