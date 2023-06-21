1adfe3511df059706d561bbfa2d7d3503c3e0396
sh4 <krzysztof.wydrzynski99@gmail.com>
2023-06-21 23:39:54 +0200

    Updated some stuff

--- src/main/java/pl/glitchguru/issuetracker/configuration/filter/JwtTokenFilter.java
+++ src/main/java/pl/glitchguru/issuetracker/configuration/filter/JwtTokenFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -19,6 +20,7 @@ import java.io.IOException;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class JwtTokenFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
@@ -31,6 +33,7 @@ public class JwtTokenFilter extends OncePerRequestFilter {
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
+        log.info("Got a request to authorize: " + request.getRequestURI());
         final String authHeader = request.getHeader("Authorization");
         if (authHeader == null ||!authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
